75f3f6e3c3385b739bc979723522fa79d5f14cf8
Ivan Pentchev <pentchev.ivan@gmail.com>
2018-08-24 00:40:32 +0300

    PrivateMessageDetail added

--- catchme.bg/catchme.bg/Data/CatchmeContext.cs
+++ catchme.bg/catchme.bg/Data/CatchmeContext.cs
@@ -14,5 +14,17 @@ namespace catchme.bg.Data
         }
 
         public virtual DbSet<MessageDetail> MessageDetails { get; set; }
+
+        public virtual DbSet<PrivateMessageDetail> PrivateMessageDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PrivateMessageDetail>(entity =>
+            {
+                entity.Property(e => e.PrivateMessageDetailId).HasColumnName("PrivateMessageDetailId");
+                entity.Property(e => e.UserNameFrom).IsRequired().HasMaxLength(256).IsUnicode(true);
+                entity.Property(e => e.UserNameTo).IsRequired().HasMaxLength(256).IsUnicode(true);
+            });
+        }
     }
 }
---
+++ catchme.bg/catchme.bg/Models/PrivateMessageDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace catchme.bg.Models
+{
+    public class PrivateMessageDetail
+    {
+        public long PrivateMessageDetailId { get; set; }
+
+        public string UserNameFrom { get; set; }
+
+        public string UserNameTo { get; set; }
+
+        public string Message { get; set; }
+    }
+}
