2456df806b80938259f4734e8b8c64a9993a6945
Ivan Pentchev <pentchev.ivan@gmail.com>
2018-12-11 21:30:17 +0200

    Mbti save works kind of. More work needed

--- catchme.bg/catchme.bg/Controllers/MbtiController.cs
+++ catchme.bg/catchme.bg/Controllers/MbtiController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using catchme.bg.Areas.Identity.Data;
 using catchme.bg.Data;
 using catchme.bg.Models;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 /// <summary>
 /// Based on the SO Question: https://stackoverflow.com/questions/12844763/getting-a-list-of-radio-button-values-in-asp-mvc-3/12845534#12845534
@@ -13,6 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace catchme.bg.Controllers
 {
+    [Authorize]
     public class MbtiController : Controller
     {
         public catchmebgContext _bgcontext { get; set; }
@@ -21,9 +24,22 @@ namespace catchme.bg.Controllers
 
         protected Evaluation Eval { get; set; }
 
-        protected CatchmebgUser CurrentUser { get; set; }
+        protected CatchmebgUser CurrentUser
+        {
+            get
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                }
+
+                return userId != null ? _bgcontext.Users.FirstOrDefault(x => x.Id == userId) : null;
+            }
+            set { _currentUser = value; }
+        }
 
         protected string userId;
+        private CatchmebgUser _currentUser;
 
         public MbtiController(catchmebgContext bgcontext, CatchmeContext context)
         {
@@ -33,34 +49,62 @@ namespace catchme.bg.Controllers
 
         public IActionResult Index()
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-
-            }
-            
-            if (userId != null)
-            {
-                CurrentUser = _bgcontext.Users.FirstOrDefault(x => x.Id == userId);
-            }
 
             Eval = new Evaluation()
             {
                 UserName = CurrentUser.UserName,
-                Questions = _context.Questions.ToList(),
-                Answers = _context.Answers.Where(u=>u.UserName==CurrentUser.UserName).ToList()
+                Questions = _context.Questions.Where(u => u.Language == "bg").OrderBy(u => u.QuestionID).ToList(),
+                Answers = new List<Answer>()
             };
+
+            CreateAnswers(Eval);
+            Eval.Answers = _context.Answers.Where(u => u.UserName == CurrentUser.UserName).ToList();
             return View(Eval);
         }
 
 
-            
+        [HttpPost]
+        public ActionResult Index(Evaluation model)
+        {
+            Eval = new Evaluation()
+            {
+                UserName = CurrentUser.UserName,
+                Questions = _context.Questions.Where(u => u.Language == "bg").OrderBy(u => u.QuestionID).ToList(),
+                Answers = new List<Answer>()
+            };
+            if (ModelState.IsValid)
+            {
+                CreateAnswers(Eval);
+                return RedirectToAction("ThankYou"); //PRG Pattern
+            }
+            //reload questions
+            return View(Eval);
+        }
+
+        private void CreateAnswers(Evaluation model)
+        {
+            foreach (var q in model.Questions)
+            {
+                var qId = q.QuestionID;
+                Answer selectedAnswer = new Answer();
+                if (model.Answers.Any())
+                {
+                    selectedAnswer = model.Answers[q.QuestionID - 1];
+                }
+
+                //Save
+                _context.Answers.Add(new Answer()
+                {
+                    QuestionID = qId,
+                    SelectedAnswer = selectedAnswer.SelectedAnswer,
+                    UserName = CurrentUser.UserName
+                });
+                _context.SaveChanges();
+            }
+        }
 
 
 
 
-
-            
     }
 }
