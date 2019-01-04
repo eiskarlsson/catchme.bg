3e3831af916ee8de3ec9ff20b7c38833e6160c28
Ivan Pentchev <pentchev.ivan@gmail.com>
2019-01-04 14:49:13 +0200

    Fixed empty message in DDLs and improved ProfileController

--- catchme.bg/catchme.bg/Controllers/ProfileController.cs
+++ catchme.bg/catchme.bg/Controllers/ProfileController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using catchme.bg.Areas.Identity.Data;
+using catchme.bg.Data;
 using catchme.bg.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +12,56 @@ namespace catchme.bg.Controllers
 {
     public class ProfileController : Controller
     {
+        public catchmebgContext _bgcontext { get; set; }
+
+        public CatchmeContext _context { get; set; }
+
+        protected ProfileViewModel ProfileViewModel { get; set; }
+
+        protected CatchmebgUser CurrentUser
+        {
+            get
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    userId = HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                }
+
+                return userId != null ? _bgcontext.Users.FirstOrDefault(x => x.Id == userId) : null;
+            }
+            set => _currentUser = value;
+        }
+
+        protected string userId;
+        private CatchmebgUser _currentUser;
+
+        public ProfileController(catchmebgContext bgcontext, CatchmeContext context)
+        {
+            _bgcontext = bgcontext;
+            _context = context;
+        }
+
+
         public IActionResult Index()
         {
             var model = new ProfileViewModel();
+            model.User = CurrentUser;
+            model.Profile = new Profile();
+            model.Profile.DateCreated = DateTime.Now;
+            model.Profile.DateLastChange = DateTime.Now;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(ProfileViewModel model)
+        {
+            model.Profile.DateCreated = DateTime.Now;
+            model.Profile.DateLastChange = DateTime.Now;
+            if (ModelState.IsValid)
+            {
+            }
+
             return View(model);
         }
     }
--- catchme.bg/catchme.bg/Models/Profile.cs
+++ catchme.bg/catchme.bg/Models/Profile.cs
@@ -14,61 +14,61 @@ namespace catchme.bg.Models
         public CatchmebgUser User { get; set; }
 
         [Display(Name = "Pets:")]
-        public int SelectedPets { get; set; }
+        public int? SelectedPets { get; set; }
 
         [Display(Name = "Children:")]
-        public int SelectedChildren { get; set; }
+        public int? SelectedChildren { get; set; }
 
         [Display(Name = "Drugs:")]
-        public int SelectedDrugs { get; set; }
+        public int? SelectedDrugs { get; set; }
 
         [Display(Name = "Diet:")]
-        public int SelectedDiet { get; set; }
+        public int? SelectedDiet { get; set; }
 
         [Display(Name = "Drinks:")]
-        public int SelectedDrinks { get; set; }
+        public int? SelectedDrinks { get; set; }
 
         [Display(Name = "Smokes:")]
-        public int SelectedSmokes { get; set; }
+        public int? SelectedSmokes { get; set; }
 
         [Display(Name = "Religion:")]
-        public int SelectedReligion { get; set; }
+        public int? SelectedReligion { get; set; }
 
         [Display(Name = "Ethnicity:")]
-        public int SelectedEthnicity { get; set; }
+        public int? SelectedEthnicity { get; set; }
 
         [Display(Name = "Education:")]
-        public int SelectedEducation { get; set; }
+        public int? SelectedEducation { get; set; }
 
         [Display(Name = "Hair Color:")]
-        public int SelectedHairColor { get; set; }
+        public int? SelectedHairColor { get; set; }
 
         [Display(Name = "Eye Color:")]
-        public int SelectedEyeColor { get; set; }
+        public int? SelectedEyeColor { get; set; }
 
         [Display(Name = "Body Type:")]
-        public int SelectedBodyType { get; set; }
+        public int? SelectedBodyType { get; set; }
 
         [Display(Name = "Weight:")]
-        public int SelectedWeight { get; set; }
+        public int? SelectedWeight { get; set; }
 
         [Display(Name = "Height:")]
-        public int SelectedHeight { get; set; }
+        public int? SelectedHeight { get; set; }
 
         [Display(Name = "Languages:")]
-        public int SelectedLanguages { get; set; }
+        public int? SelectedLanguages { get; set; }
 
         [Display(Name = "Gender:")]
-        public int SelectedGender { get; set; }
+        public int? SelectedGender { get; set; }
 
         [Display(Name = "Age:")]
-        public int SelectedAge { get; set; }
+        public int? SelectedAge { get; set; }
 
         [Display(Name = "Looking For:")]
-        public int SelectedLookingFor { get; set; }
+        public int? SelectedLookingFor { get; set; }
 
         [Display(Name = "Marital Status:")]
-        public int SelectedMaritalStatus { get; set; }
+        public int? SelectedMaritalStatus { get; set; }
 
         public string EducationDescription { get; set; }
         public string JobDescription { get; set; }
