921338a1b9d8be7f65f4507fffc1ae776e9dece1
Ivan Pentchev <pentchev.ivan@gmail.com>
2019-01-12 09:52:22 +0200

    Fixed version mismatches

--- catchme.bg/catchme.bg/Program.cs
+++ catchme.bg/catchme.bg/Program.cs
@@ -19,6 +19,8 @@ namespace catchme.bg
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseSetting("detailedErrors", "true")
+                .UseStartup<Startup>()
+                .CaptureStartupErrors(true);
     }
 }
