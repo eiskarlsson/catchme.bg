4013602c238bdf2f56d3f9780ac9d6f46b7ce5ce
Ivan Pentchev <pentchev.ivan@gmail.com>
2019-03-09 21:37:10 +0200

    Data annotations for Autoincrement added. Not sure if it works

--- catchme.bg/catchme.bg/Models/Filter.cs
+++ catchme.bg/catchme.bg/Models/Filter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace catchme.bg.Models
 {
     public class Filter
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
         [Required]
         public string FilterUserId { get; set; }
--- catchme.bg/catchme.bg/Models/IProfileItem.cs
+++ catchme.bg/catchme.bg/Models/IProfileItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace catchme.bg.Models
 {
     public interface IProfileItem
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         int Id { get; set; }
         int ItemId { get; set; }
         string Name { get; set; }
