90847dfbeaef233883e538798f2222b6fc1f7529
Ivan Pentchev <pentchev.ivan@gmail.com>
2019-09-03 15:23:36 +0300

    Privacy Policy and Data Protection pages added.

---
+++ catchme.bg/catchme.bg/Areas/Legal/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace catchme.bg.Areas.Legal.Controllers
+{
+    [Area("Legal")]
+    public class HomeController : Controller
+    {
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+        public IActionResult DataProtection()
+        {
+            return View();
+        }
+    }
+}
